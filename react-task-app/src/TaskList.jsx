2c555776227cf60e8132312159f382f43ceb818c
SuperGandhi <patrickborjabueno@hotmail.com>
2022-10-20 13:30:32 -0500

    Add task data and hooks functionalities!

--- react-task-app/src/TaskList.jsx
+++ react-task-app/src/TaskList.jsx
@@ -1,9 +1,32 @@
 import React from 'react'
+import {tasks as data} from './tasks.js';
+import {useState, useEffect} from 'react';
+
+
+
 
 const TaskList = () => {
-  return (
-    <div>TaskList</div>
-  )
+
+    const [tasks, setTasks] = useState([])
+
+    useEffect(() => {
+        setTasks(data)
+    }, [])
+    
+
+    if(tasks.length === 0 ) return <h1>Dont tasks yet</h1>
+    return (
+        <div>
+            {
+                tasks.map(task => (<div>
+                    <div key={task.id}>
+                        <h1>{task.title}</h1>
+                        <p>{task.description}</p>
+                    </div>
+                </div>))
+            }
+        </div>
+    )
 }
 
 export default TaskList
---
+++ react-task-app/src/tasks.js
@@ -0,0 +1,34 @@
+
+// para poder importarlos primero debo exportarlos -- > 'export'
+export const tasks = [
+    {
+        id: 0,
+        title: 'my firts task',
+        description: 'my firts task about ...'
+    },
+    {
+        id: 1,
+        title: 'my second task',
+        description: 'my firts task about ...'
+    },
+    {
+        id: 2,
+        title: 'my third task',
+        description: 'my firts task about ...'
+    },
+    {
+        id: 3,
+        title: 'my firts task',
+        description: 'my firts task about ...'
+    },
+    {
+        id: 4,
+        title: 'my firts task',
+        description: 'my firts task about ...'
+    },
+    {
+        id: 5,
+        title: 'my firts task',
+        description: 'my firts task about ...'
+    },
+]
